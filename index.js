8136b88cf86e33997695a98e885e75f8fc0c9ceb
Mark Workman <mark-workman@lambdastudents.com>
2020-10-02 19:51:34 -0400

    Started project

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ console.log('its working');
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
 function getFinals(/* code here */) {
-
-    /* code here */
-
+    console.log(test_code);
 };
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
